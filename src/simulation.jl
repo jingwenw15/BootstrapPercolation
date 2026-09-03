dc22a60fd6d67f7ffd265ec7a9e8ec3caec91b18
Jingwen Wu <jingwenw15@gmail.com>
2026-09-03 15:42:45 -0700

    add comments/small re structure

--- src/simulation.jl
+++ src/simulation.jl
@@ -1,30 +1,38 @@
-using DataStructures
-
-grid = ["." "X" "." ; "X" "." "X"; "." "X" "."]
-
 function simulate(grid)
     queue = Queue{CartesianIndex{2}}()
     freq = DefaultDict{CartesianIndex{2}, Int}(0)
 
     R, C = size(grid)
 
-    offsets = (CartesianIndex(1, 0), CartesianIndex(-1, 0), CartesianIndex(0, 1), CartesianIndex(0, -1))
+    offsets = (
+        CartesianIndex(1, 0),
+        CartesianIndex(-1, 0),
+        CartesianIndex(0, 1),
+        CartesianIndex(0, -1)
+    )
 
-    for j in 1:C, i in 1:R 
+    state = copy(grid)
+
+    # Find cells that should become infected at time 1
+    for j in 1:C, i in 1:R
         cur = CartesianIndex(i, j)
 
-        if grid[cur] == "."
+        if state[cur] == "."
             infectedNeighbors = 0
 
             for offset in offsets
-                neighbor = cur + offset 
+                neighbor = cur + offset
 
-                if 1 <= neighbor[1] <= R && 1 <= neighbor[2] <= C && grid[neighbor] == "X"
+                if 1 <= neighbor[1] <= R &&
+                   1 <= neighbor[2] <= C &&
+                   state[neighbor] == "X"
+
                     infectedNeighbors += 1
                 end
-            end 
+            end
 
             freq[cur] = infectedNeighbors
+
             if infectedNeighbors >= 2
                 enqueue!(queue, cur)
             end
@@ -33,17 +41,21 @@ function simulate(grid)
 
     time = 0
 
+    # Process infection rounds
     while !isempty(queue)
         levelSize = length(queue)
 
         for _ in 1:levelSize
             cur = dequeue!(queue)
-            grid[cur] = "X"
+            state[cur] = "X"
 
             for offset in offsets
-                neighbor = cur + offset 
+                neighbor = cur + offset
 
-                if 1 <= neighbor[1] <= R && 1 <= neighbor[2] <= C && grid[neighbor] == "."
+                if 1 <= neighbor[1] <= R &&
+                   1 <= neighbor[2] <= C &&
+                   state[neighbor] == "."
+
                     freq[neighbor] += 1
 
                     if freq[neighbor] == 2
@@ -56,5 +68,7 @@ function simulate(grid)
         time += 1
     end
 
-    return grid, all(grid .== "X"), time
+    percolated = all(state .== "X")
+
+    return state, percolated, time
 end
